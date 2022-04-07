13288f31d70ae2f425c29acf96e9d040fae0919f
Andre Ferreira <andre.souza@sensedia.com>
2022-04-07 16:29:35 -0300

    Added eventId attribute

--- src/main/java/br/dev/multicode/models/OrderPaymentMessage.java
+++ src/main/java/br/dev/multicode/models/OrderPaymentMessage.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class OrderPaymentMessage {
 
+  private UUID eventId;
   private UUID orderId;
   private UUID userId;
   private TypePayment typePayment;
